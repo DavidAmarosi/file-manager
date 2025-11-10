c347eb5cd3bf5b931cad8c55339c5ef9c7691ba8
david amar <dwdmr6143@gmail.com>
2025-11-10 11:44:46 +0200

    method str

--- book.py
+++ book.py
@@ -1,12 +1,15 @@
 class Book:
-  ISBN_COUNT = 1000 
+    ISBN_COUNT = 1000 
 
-  def __init__(self, title, author):
+    def __init__(self, title, author):
         self.title = title
         self.author = author
         self.ISBN = Book.ISBN_COUNT
         Book.ISBN_COUNT += 1
         self.is_available = True
+
+    def __str__(self):
+        return f"Title : {self.title}, Author : {self.author}, ISBN : {self.ISBN}, is_availble {self.is_available}"
 
 class Library:
     def __init__(self):
@@ -19,9 +22,22 @@ class Library:
         for book in self.library:
             print(book.title)
 
+    def print_books(self):
+        for book in self.library:
+            print(book)
+
+    def __str__(self):
+        title_list = [book for book in self.library]
+        return str(title_list)
+
+
+
 library = Library()
 library.add_book("brachot","rav ashi")
 library.add_book("Shabat","rav ashi")
 library.add_book("gitin","rav ashi")
 
+# library.print_books()
+
+print(library)
 library.print_books()
